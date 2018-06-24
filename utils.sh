513bd85fb562b0b924b3bf9eaf4193ee02ff72d9
Noel Colon <ncolon20@gmail.com>
2018-06-24 16:31:12 -0400

    Add util helpers

---
+++ utils.sh
@@ -0,0 +1,29 @@
+# Author: Noel Colon
+# Description: Reuseable util helpers for scripting
+
+# Text styling
+bold=$(tput bold) # Bold
+smul=$(tput smul) # Underline start
+rmul=$(tput rmul) # Underline end
+smso=$(tput smso) # Standout start
+rmso=$(tput rmso) # Standout end
+normal=$(tput sgr0) # Normalize text
+# Foreground coloring
+red=$(tput setaf 1)
+green=$(tput setaf 46)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+
+print_y(){
+	printf "${yellow}%s${normal}" "$@"
+}
+
+copy_to_clipboard() {	
+	if [[ $(uname) == 'Darwin'  ]]; then
+		echo "$@" | pbcopy
+		printf "Copied: ${yellow}%s${normal}\n" "$@"
+	fi
+}
+
+
